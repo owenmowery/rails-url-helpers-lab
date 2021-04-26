62dbdcdf310b9bfe377ac2fc1578d986c7d10d1c
Owen Mowery <owenmowery99@gmail.com>
2021-04-26 12:56:47 -0400

    commit tree 9b2cee50eab43349c2a0ac1314062eccccfc5d88

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: [:show, :activate]
   
   def index
     @students = Student.all
@@ -8,8 +8,17 @@ class StudentsController < ApplicationController
   def show
   end
 
-  private
+  def activate
+    if @student.active 
+      @student.active = false
+    else
+      @student.active = true
+    end 
+    @student.save
+    redirect_to student_path(@student)
+  end
 
+  private
     def set_student
       @student = Student.find(params[:id])
     end
